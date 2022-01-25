d0ad6d450e8bd48c3cff336e2274bd9fcb3de268
kevnjs <91511588+kevnjs@users.noreply.github.com>
2022-01-24 20:13:27 -0600

    Chech

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -3,9 +3,37 @@ import '@testing-library/jest-dom/extend-expect';
 import { render, screen } from '@testing-library/react';
 import Episode from './../Episode';
 
+const episode = {
+    id: 1,
+    image: null,
+    name: 'Strangers',
+    season: 2,
+    number: 1,
+    summary: "An overrated show",
+    runtime: '1 hour'
+}
 
-test("renders without error", () => {});
+test("renders without error", () => {
+    render(<Episode episode={episode}/>)
+});
 
-test("renders the summary test passed as prop", ()=>{});
+test("renders the summary test passed as prop", ()=>{
+    render(<Episode episode={episode} />)
 
-test("renders default image when image is not defined", ()=>{});
+    const summary = screen.getByText("An overrated show")
+    
+    expect(summary).toBeInTheDocument()
+    expect(summary).toHaveTextContent("An overrated show")
+    expect(summary).toBeTruthy()
+});
+
+test("renders default image when image is not defined", ()=>{
+    render(<Episode episode={episode}/>)
+    
+    const defaultImg = screen.getByRole('img')
+    const altText = screen.getByAltText('https://i.ibb.co/2FsfXqM/stranger-things.png')
+
+    expect(defaultImg).toBeInTheDocument()
+    expect(altText).toBeInTheDocument()
+    expect(defaultImg).toHaveAttribute('alt', 'https://i.ibb.co/2FsfXqM/stranger-things.png')
+});
--- src/components/tests/Show.test.js
+++ src/components/tests/Show.test.js
@@ -2,15 +2,38 @@ import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-
 import Show from './../Show';
 
-test('renders without errors', ()=>{});
+const showsTest = [{
+        name: "Strange Show",
+        summary: "An overrated show",
+        seasons: [{
+            id: 0,
+            name: 'season 1',
+            episodes: []
+        }, 
+        {
+            id: 1,
+            name: 'season 2',
+            episodes: []
+        }]
+}]
 
-test('renders Loading component when prop show is null', () => {});
+
+test('renders without errors', ()=> {
+    render(<Show show={showsTest}/>)
+});
+
+test('renders Loading component when prop show is null', () => {
+    render(<Show show={null}/>)
+    const loading = screen.getByText("Fetching data...")
+    expect(loading).toBeInTheDocument()
+});
 
 
-test('renders same number of options seasons are passed in', ()=>{});
+test('renders same number of options seasons are passed in', ()=>{
+    render(<Show />)
+});
 
 test('handleSelect is called when an season is selected', () => {});
 
